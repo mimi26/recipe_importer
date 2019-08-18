74aaed2d3926c3d161545b62600f1a5f97de2ec2
mimi klein <lifeistoogoodtobetrue@gmail.com>
2019-08-17 23:52:54 -0400

    send ingredients from 3 example sites

--- middleware/fetch_recipes.js
+++ middleware/fetch_recipes.js
@@ -3,11 +3,25 @@ const puppeteer = require('puppeteer');
 const getRecipeName = async url => {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
-    const cookies = await page.goto(url);
-    await page.waitForSelector('h1');
+    await page.goto(url);
     const recipesData = await page.evaluate(() => {
-        const name = document.querySelector('h1').textContent;
-        return { name };
+        const name = document.querySelector('h1').textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
+        const ingredients = [];
+        let ingredientNodeList;
+        // grab eatthelove.com ingredients
+        if (document.querySelectorAll('span[itemprop="ingredients"]').length !== 0) {
+            ingredientNodeList = document.querySelectorAll('span[itemprop="ingredients"]');
+        // grab laurainthekitchen.com ingredients
+        } else if (document.querySelector('#recipe-ingredients')) {
+            ingredientNodeList = document.querySelectorAll('#recipe-ingredients li');
+        // grab cooking.nytimes.com ingredients
+        } else if (document.querySelectorAll('li[itemprop="recipeIngredient"]').length !== 0) {
+            ingredientNodeList = document.querySelectorAll('li[itemprop="recipeIngredient"]');
+        }
+        for (let i = 0; i < ingredientNodeList.length; i++) {
+            ingredients.push(ingredientNodeList[i].textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
+        }
+        return { name, ingredients };
     });
     await browser.close();
     return recipesData;

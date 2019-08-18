ff4b47703fda21c415af54c80f2596fa5b8d85d3
mimi klein <lifeistoogoodtobetrue@gmail.com>
2019-08-18 14:06:17 -0400

    fetch laurainthekitchen and maangchi steps

--- middleware/fetch_recipes.js
+++ middleware/fetch_recipes.js
@@ -7,24 +7,33 @@ const getRecipeName = async url => {
     const recipesData = await page.evaluate(() => {
         const name = document.querySelector('h1').textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim();
         const ingredients = [];
+        let steps = [];
         let ingredientNodeList;
+        let stepsNodeList;
         // grab eatthelove.com ingredients
         if (document.querySelectorAll('span[itemprop="ingredients"]').length !== 0) {
             ingredientNodeList = document.querySelectorAll('span[itemprop="ingredients"]');
-        // grab laurainthekitchen.com ingredients
+        // grab laurainthekitchen.com ingredients and steps
         } else if (document.querySelector('#recipe-ingredients')) {
             ingredientNodeList = document.querySelectorAll('#recipe-ingredients li');
+            steps = document.querySelector('#recipe-process ul').textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim().split(/[1-9]\)/);
+            // remove empty string at first index
+            steps.shift();
+            // steps = document.querySelector('#recipe-process ul').textContent.split(/[1-9]\)/);
         // grab cooking.nytimes.com ingredients
         } else if (document.querySelectorAll('li[itemprop="recipeIngredient"]').length !== 0) {
             ingredientNodeList = document.querySelectorAll('li[itemprop="recipeIngredient"]');
-        // grab maangchi.com ingredients
+        // grab maangchi.com ingredients and steps
         } else {
+            const recipeDiv = document.querySelector('.entry.clearfix').children;
             ingredientNodeList = document.querySelectorAll('h4 + ul li');
+            stepsNodeList = Array.prototype.slice.call(recipeDiv).slice(12, 18);
+            steps = stepsNodeList.map(node => node.textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
         }
         for (let i = 0; i < ingredientNodeList.length; i++) {
             ingredients.push(ingredientNodeList[i].textContent.replace(/[\n\r]+|[\s]{2,}/g, ' ').trim());
         }
-        return { name, ingredients };
+        return { name, ingredients, steps };
     });
     await browser.close();
     return recipesData;
